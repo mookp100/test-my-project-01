ce6e2f10c16c3beabfd5181d186cfd2b6dea12eb
mookp100 <nrathipkaewsrixxn@gmail.com>
2026-06-18 12:53:23 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = requier("expess"); //นำเข้า express เพื่อสร้าง
+const path = require("path");
+const app = express(); 
+
+app.use(express.json()); 
+
+app.get("/students", (req, res) =>{
+    res.json([
+        { id: 1, name: "John Doe",},
+    ])
+})    
+
+app.listen(3000, () => {
+    console.log("Server started");
+})
